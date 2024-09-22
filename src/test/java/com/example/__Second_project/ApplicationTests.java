061e8c24c50be187cc2cfe20339de5da1fcbf6f4
AltmasIson <altmash.m2024@gmail.com>
2024-09-22 19:07:09 +0530

    Intial File

---
+++ src/test/java/com/example/__Second_project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__Second_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
